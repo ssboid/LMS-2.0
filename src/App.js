13c5f30aa49e0d5c84bc4b79bbe262c5a21f1ad8
Sunit Shakya <shakyasunit11@gmail.com>
2024-05-16 08:03:35 +0545

    next

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 import Home from "./pages/Home";
 import Layout from "./pages/theme/Layout";
 import Browse from "./pages/Browse";
+import Book from "./pages/Book";
 import SearchResult from "./pages/SearchResult";
 function App() {
   return (
     <div>
-      {/* hello */}
-      {/* new */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />}></Route>
 
             <Route path="/browse" element={<Browse />} />
+            <Route path="/book" element={<Book />} />
             <Route path="/search" element={<SearchResult />} />
           </Route>
         </Routes>
--- src/pages/theme/Layout.js
+++ src/pages/theme/Layout.js
@@ -10,7 +10,6 @@ const Layout = () => {
           <Outlet />
       </div>
       <Footer />
-      <Footer />
       
 
     </div>
