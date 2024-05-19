e5d0d5651995b0ea4502064ff211ceb8d1e4a26a
Sunit Shakya <shakyasunit11@gmail.com>
2024-05-19 18:07:26 +0545

    again

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,9 +1,34 @@
 import React from "react";
 
 const Browse = () => {
+  const count = 20;
+  const items = Array.from({ length: count }, (_, index) => index + 1);
+
   return (
     <div>
-      <div className="pagebox">a</div>
+      <div className="pagebox">
+        <div className="filterbox">
+          <div className="filtercrit">
+            a
+          </div>
+        </div>
+        <div className="browsing">
+          {items.map((item, index) => (
+            <div key={index}>
+              <div className="browsingcell">
+                <div>
+                  <img
+                    className="browsedimage"
+                    src="https://via.placeholder.com/210x270"
+                    alt="Dummy Image"
+                  />
+                </div>
+                <div className="browsedtext">name</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const SearchResult = () => {
+  const count = 20;
+  const items = Array.from({ length: count }, (_, index) => index + 1);
   return (
     <div>
       <div className='pagebox'>
-        a
+      <div className="browsing">
+          {items.map((item, index) => (
+            <div key={index}>
+              <div className="browsingcell">Content to be repeated</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
